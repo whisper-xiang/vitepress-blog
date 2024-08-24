ce63f1b455373d8ae5382657120741fcbdb670f3
whisper-xiang <xiangergou000@gmail.com>
2024-08-24 20:37:25 +0800

    docs: add '一文读懂web3.0'

--- docs/.vitepress/configs/sidebar.ts
+++ docs/.vitepress/configs/sidebar.ts
@@ -133,6 +133,7 @@ export const sidebar: DefaultTheme.Config['sidebar'] = {
       items: [
         { text: '洞穴奇案', link: '/notes&thoughts/essay/洞穴奇案' },
         { text: '哲学导论', link: '/notes&thoughts/essay/哲学导论' },
+        { text: '一本书读懂Web3.0', link: '/notes&thoughts/essay/一本书读懂Web3.0' },
       ],
     },
   ],
